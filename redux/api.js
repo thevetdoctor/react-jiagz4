2c2a5499c7e4b155df5d58d4f478fee7c6867d67
thevetdoctor <thevetdoctor@gmail.com>
2019-10-29 21:56:03 +0100

    2 files modified

--- redux/api.js
+++ redux/api.js
@@ -1,13 +1,11 @@
 export const apiData = async (user) => {
   
-  let url = `https://us-central1-quickstart-1559031126554.cloudfunctions.net/createNewUser?f=${user.firstname}&l=${user.lastname}&b=${user.birthday}&a=${user.age}&h=${user.hobby}`;
-  try {
-    const res = await fetch(url, { method: 'GET', headers: { 'Content-Type': 'application/json' }});
+  // let url = `https://us-central1-quickstart-1559031126554.cloudfunctions.net/createNewUser?f=${user.firstname}&l=${user.lastname}&b=${user.birthday}&a=${user.age}&h=${user.hobby}`;
 
-    // ("https://jsonplaceholder.typicode.com/users", {
-    //   cors: "no-cors",
-    //   headers: { "Content-Type": "application/json" }
-    // });
+  let url = "https://jsonplaceholder.typicode.com/users";
+
+  try {
+    const res = await fetch(url, { method: 'GET', cors: "no-cors", headers: { 'Content-Type': 'application/json' }});
 
     const json = await res.json();
     console.log('json response', json);
